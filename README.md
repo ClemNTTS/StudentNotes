856805cd3f2848484b08901489d93e263a2d530f
cnuttens <clement.nutt@gmail.com>
2024-11-22 12:21:46 +0100

    Student Notes

---
+++ README.md
@@ -0,0 +1,72 @@
+# Student Notes Management System
+
+This Rust program is a system for managing student notes. It allows you to add, delete, update, and retrieve student notes. The program uses a HashMap to store the notes of the students.
+
+## Features
+
+- Add a new student note.
+- Delete a student note.
+- Get a student note.
+- Update a student note.
+- Preview class notes for all students.
+- Calculate the mean of class notes.
+
+## How to Use the Program
+
+1. Run the program using the `cargo run` command.
+2. You will be presented with a menu of options:
+   - `1` to add a student note.
+   - `2` to delete a student note.
+   - `3` to get a student note.
+   - `4` to update a student note.
+   - `5` to preview class notes for all students.
+   - `6` to calculate the mean of class notes.
+   - `7` to exit the program.
+3. Choose an option by entering the corresponding number.
+4. Follow the on-screen instructions to provide the necessary information.
+
+## Usage Examples
+
+- To add a student note:
+
+  - Choose option `1`.
+  - Enter the student's name.
+  - Enter the student's note.
+
+- To delete a student note:
+
+  - Choose option `2`.
+  - Enter the name of the student whose note you want to delete.
+
+- To get a student note:
+
+  - Choose option `3`.
+  - Enter the name of the student whose note you want to retrieve.
+
+- To update a student note:
+
+  - Choose option `4`.
+  - Enter the name of the student whose note you want to update.
+  - Enter the new note for the student.
+
+- To preview class notes for all students:
+
+  - Choose option `5`.
+  - The notes for all students will be displayed.
+
+- To calculate the mean of class notes:
+  - Choose option `6`.
+  - The mean of class notes will be displayed.
+
+## Notes
+
+- Make sure to enter valid values for notes.
+- The program handles errors appropriately and provides error messages in case of issues.
+
+## Author
+
+This program was developed by ClemNTTS, Zone01 programming school.
+
+Feel free to contribute, report issues, or suggest improvements on the associated GitHub repository: [GitHub Repository Link](https://github.com/ClemNTTS/StudentNotes).
+
+Enjoy managing student notes with this Rust program!
---
+++ src/main.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+use std::io;
+
+///Add a new student with the following note to the map of students
+fn add_student(notes: &mut HashMap<String, i32>, name: String, value: i32) {
+    notes.insert(name, value);
+}
+
+///Delete a student from the map of students
+fn remove_student(notes: &mut HashMap<String, i32>, name: String) {
+    if notes.contains_key(&name) {
+        notes.remove(&name);
+    } else {
+        println!("Student not found in the class.");
+    }
+}
+
+///Get the student note for the given name
+fn get_student(notes: &mut HashMap<String, i32>, name: String) -> (String, i32) {
+    match notes.get(&name) {
+        Some(&note) => (name, note),
+        None => (name, -1), // Return -1 if the student is not found in the hashmap
+    }
+}
+
+///Update the student note by the given value
+fn update_student_note(notes: &mut HashMap<String, i32>, name: String, value: i32) {
+    if notes.contains_key(&name) {
+        notes.insert(name, value);
+    } else {
+        println!("Student not found in the class.");
+    }
+}
+
+///Print all notes for all students
+fn preview_class_notes(notes: &mut HashMap<String, i32>) {
+    println!("Class Notes:");
+    for (name, note) in notes.iter() {
+        println!("{} have the following note : {}", name.trim(), note)
+    }
+}
+
+///Return the mean of class notes
+fn mean(notes: &HashMap<String, i32>) -> f32 {
+    notes.values().sum::<i32>() as f32 / notes.len() as f32
+}
+
+fn main() {
+    let mut class_notes: HashMap<String, i32> = HashMap::new();
+
+    loop {
+        print!("");
+        println!("1. Add a student note");
+        println!("2. Delete a student note");
+        println!("3. Get student note");
+        println!("4. Update student note");
+        println!("5. Preview class notes");
+        println!("6. Calculate class mean");
+        println!("7. Exit");
+        println!("Choose an option: ");
+        let input = {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            input
+        };
+
+        match input.trim() {
+            "1" => {
+                println!("Enter the name of the student : ");
+                let mut student_name = String::new();
+                io::stdin()
+                    .read_line(&mut student_name)
+                    .expect("Failed to read name");
+                println!("Enter the note : ");
+                let mut value = String::new();
+                io::stdin().read_line(&mut value).expect("Failed to read");
+                let i_value = value.trim().parse::<i32>().unwrap_or(-1);
+                if i_value == -1 {
+                    println!("Invalid note value. Please enter a valid number.");
+                    continue;
+                }
+                add_student(&mut class_notes, student_name, i_value);
+            }
+            "2" => {
+                println!("Enter the name of the student : ");
+                let mut student_name = String::new();
+                io::stdin()
+                    .read_line(&mut student_name)
+                    .expect("Failed to read name");
+                remove_student(&mut class_notes, student_name);
+            }
+            "3" => {
+                println!("Enter the name of the student : ");
+                let mut student_name = String::new();
+                io::stdin()
+                    .read_line(&mut student_name)
+                    .expect("Failed to read name");
+                let (name, note) = get_student(&mut class_notes, student_name);
+                if note == -1 {
+                    println!("Student not found in the class.");
+                } else {
+                    println!("{} have the following note : {}", name.trim(), note);
+                }
+            }
+            "4" => {
+                println!("Enter the name of the student : ");
+                let mut student_name = String::new();
+                io::stdin()
+                    .read_line(&mut student_name)
+                    .expect("Failed to read name");
+                println!("Enter the new note : ");
+                let mut value = String::new();
+                io::stdin().read_line(&mut value).expect("Failed to read");
+                let i_value = value.trim().parse::<i32>().unwrap_or(-1);
+                if i_value == -1 {
+                    println!("Invalid note value. Please enter a valid number.");
+                    continue;
+                }
+                update_student_note(
+                    &mut class_notes,
+                    student_name,
+                    value.trim().parse().expect("Failed to parse"),
+                );
+            }
+            "5" => preview_class_notes(&mut class_notes),
+            "6" => {
+                let mean_note = mean(&class_notes);
+                println!("The mean of class notes is : {}", mean_note);
+            }
+
+            "7" => break,
+            _ => println!("Option invalide"),
+        }
+    }
+}
